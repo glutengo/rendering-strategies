f40e86dd26d4831f372d919143567f4bd11a3c17
Markus Glutting <markus.glutting@student.htw-berlin.de>
2020-02-20 14:25:19 +0100

    Add angular TransferHttpCacheModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { BackendBaseUrlProvider } from './config';
     PostListModule,
     PostModule,
     HeaderModule,
-    NotFoundModule
+    NotFoundModule,
+    TransferHttpCacheModule
   ],
   providers: [
     BackendBaseUrlProvider
