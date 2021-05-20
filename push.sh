5cda6716650fe37d26314e8bc093d703c23888c3
Markus Glutting <markus.glutting@student.htw-berlin.de>
2021-05-20 17:16:17 +0200

    add branch for angular-csr deployment

---
+++ push.sh
@@ -0,0 +1,3 @@
+ #!/usr/bin/env bash
+
+ git push origin `git subtree split --prefix angular-blog deployment/angular-csr`:deployment/angular-csr --force
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BACKEND_URL: 'http://rendering-strategies-786862282.eu-central-1.elb.amazonaws.com:8082'
+  BACKEND_URL: 'https://rendering-strategies.herokuapp.com'
 };
